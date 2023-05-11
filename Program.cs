d01d8ff9af981315e73161b9439c92361d19ab3b
Violetta <94271862+burlakova-veta@users.noreply.github.com>
2023-05-11 20:35:56 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            Console.WriteLine("Введите 3 целых числа");
+            for (int i = 0; i < 3; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                
+                if (numbers[i] < 0)
+                {
+                    numbers[i] = numbers[i] * numbers[i];
+                }
+                else if (numbers[i] > 0)
+                {
+                    numbers[i] = numbers[i] * numbers[i] * numbers[i];
+                }
+            }
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
